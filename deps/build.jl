25668046040c4cecc5cee98efc1d036506de5240
joshday <emailjoshday@gmail.com>
2021-02-11 10:19:01 -0500

    wip

--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,10 @@
 # Download links in `assets.csv`
 
+dir = joinpath(@__DIR__, "Pluto", "frontend", "offline_assets")
+mkpath(dir)
+
 for url in readlines(joinpath(@__DIR__, "assets.csv") )
     @info "Downloading: $url"
-    file = touch(joinpath(@__DIR__, "Pluto", "frontend", "offline_assets", basename(url)))
+    file = touch(dir, basename(url))
     download(url, file)
 end
