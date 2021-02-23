b1207837f9f0758dbefe508e242f7e77a4071199
joshday <emailjoshday@gmail.com>
2021-02-23 11:12:02 -0500

    update build

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,7 @@
 # Download links in `assets.csv`
 
 dir = joinpath(@__DIR__, "Pluto", "frontend", "offline_assets")
+rm(dir, force=true, recursive=true)
 mkpath(dir)
 
 for url in readlines(joinpath(@__DIR__, "assets.csv") )
